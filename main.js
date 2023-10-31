d7f04359903494b45c3738750d5ce834c73206d7
Mateus Ávila <mateusmenavila@gmail.com>
2023-10-31 18:27:44 -0300

    site v1

--- main.js
+++ main.js
@@ -2,11 +2,9 @@ const checkbox = document.querySelector("#check");
 const listaMenu = document.querySelector("#listaMenu");
 
 checkbox.addEventListener("change", function () {
-    if (checkbox.checked == true) {
+    if (checkbox.checked) {
         listaMenu.classList.remove('inativa');
-        listaMenu.classList.add('ativa');
     } else {
-        listaMenu.classList.remove('ativa');
         listaMenu.classList.add('inativa');
     }
 });
